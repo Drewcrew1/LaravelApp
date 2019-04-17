a636639aeb8389930dc381db75d7d9c3e645a02c
Drew <drewharper29@gmail.com>
2019-04-17 09:37:32 -0400

    https

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://thawing-dawn-37988.herokuapp.com/css/app.css" rel="stylesheet">
 </head>
 <body>
     <div >
